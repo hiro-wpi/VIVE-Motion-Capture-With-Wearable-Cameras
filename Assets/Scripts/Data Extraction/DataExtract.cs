c1ea3932203b966cbc592c89ea81e6a1bde6ad32
Zhuoyun Zhong <zzy905954450@gmail.com>
2022-01-09 17:59:28 -0500

    add data recorder

---
+++ Assets/Scripts/Data Extraction/DataExtract.cs
@@ -0,0 +1,261 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ExperimentDataRecord;
+using OfficeOpenXml;
+using System.IO;
+using ViveSR.anipal.Eye;
+using System;
+//using HTC.UnityPlugin.Vive;
+using Valve.VR;
+
+public class DataExtract : MonoBehaviour
+{
+    // Trial Count For Sheet Save
+    public int trialCount = 1;
+
+    // RecordFlag
+    public bool IsRecording = false;
+
+    // Frame
+    public float updateRate = 10;
+
+    // Gaze
+    public double gazeDurationThreshold;    // Threshold for gaze duration record
+    public GameObject gazeRay;
+    public GameObject gazeTime;
+    private double lastGazeTime;
+    public Vector2 lastPixel;
+
+    // Tracker For Pose
+    public GameObject[] TrackerList;
+
+    // ExcelFile
+    public string ExcelFilePath;
+    public string ExcelSheetName;
+    private FileInfo newFile;
+    public ExcelPackage exlpackage;
+    ExcelWorksheet worksheet;
+    public int raw;
+    public int col;
+
+    // Screenshot
+    public string ScreenshotDirPath;
+    public string ScreenshotDemoName;
+    public Camera cam;
+    public Rect rect;
+
+    // Time Recording
+    public System.Diagnostics.Stopwatch stopwatch;
+
+    // Record Icon
+    public GameObject startIcon;
+    public GameObject stopIcon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        stopwatch = new System.Diagnostics.Stopwatch();
+        stopwatch.Start();
+
+        lastGazeTime = 0;
+        lastPixel.x = -1;
+        lastPixel.y = -1;
+
+        var strName = ExcelFilePath.Split('/');
+        ScreenshotDemoName = strName[strName.Length - 1].Split('.')[0];
+
+        DataRecordUtil.DeleteFile(ExcelFilePath);
+        newFile = new FileInfo(Application.dataPath + "/" + ExcelFilePath);
+        exlpackage = new ExcelPackage(newFile);
+        InitialExcelSheet(exlpackage, ExcelFilePath, ExcelSheetName + trialCount.ToString(), out worksheet);
+        raw = 2;
+        col = 1;
+
+        DataRecordUtil.DeleteDirectory(ScreenshotDirPath);
+        
+        // Call Function to collect Data every 10 times a second
+        InvokeRepeating("UpdateData", 1f, 1 / updateRate);
+
+    }
+
+    // Collect Data
+    void UpdateData()
+    {
+        if (IsRecording)
+        {
+            worksheet.Cells[raw, col++].Value = Time.time;
+            worksheet.Cells[raw, col++].Value = lastPixel.x.ToString();
+            worksheet.Cells[raw, col++].Value = lastPixel.y.ToString();
+            worksheet.Cells[raw, col++].Value = lastGazeTime.ToString();
+
+            for (int i = 0; i < TrackerList.Length; i++)
+            {
+                var tf = TrackerList[i].GetComponent<Transform>();
+                worksheet.Cells[raw, col++].Value = tf.position.x;
+                worksheet.Cells[raw, col++].Value = tf.position.y;
+                worksheet.Cells[raw, col++].Value = tf.position.z;
+                worksheet.Cells[raw, col++].Value = tf.rotation.eulerAngles.x;
+                worksheet.Cells[raw, col++].Value = tf.rotation.eulerAngles.y;
+                worksheet.Cells[raw, col++].Value = tf.rotation.eulerAngles.z;
+            }
+            raw++;
+            col = 1;
+        }
+    }
+
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        // Start or stop recording
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            if (IsRecording == true)
+            {
+                startIcon.SetActive(false);
+                stopIcon.SetActive(true);
+                Debug.Log("Recording Off...");
+                Debug.Log("Saving Data To Excel File!");
+                exlpackage.Save();
+                Debug.Log("Data has been saved successfully!");
+            }
+            else
+            {
+                startIcon.SetActive(true);
+                stopIcon.SetActive(false);
+                Debug.Log("Recording On...");
+            }
+            IsRecording = !IsRecording;
+        }
+
+        // Save data to file
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Debug.Log("Saving Data To Excel File!");
+            exlpackage.Save();
+            Debug.Log("Data has been saved successfully!");
+        }
+
+        // Respawn a new sheet for new trial in each specific task
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            Debug.Log("Trial " + trialCount.ToString() + " has ended, saving the data to excel sheet Now...");
+            exlpackage.Save();
+            DataRecordUtil.TakeScreenshot(ScreenshotDirPath, ScreenshotDemoName, trialCount);
+            trialCount++;
+            Debug.Log("Trial " + trialCount.ToString() + " starts at another sheet");
+            //DataRecordUtil.SheetRespawn(exlpackage, ExcelSheetName + trialCount.ToString(), out worksheet);
+            InitialExcelSheet(exlpackage, ExcelFilePath, ExcelSheetName + trialCount.ToString(), out worksheet);
+            //IsRecording = false;
+            raw = 2;
+            col = 1;
+        }
+
+        // Recording to another file
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            trialCount = 1;
+            DataRecordUtil.DeleteFile(ExcelFilePath);
+            newFile = new FileInfo(Application.dataPath + "/" + ExcelFilePath);
+            exlpackage = new ExcelPackage(newFile);
+            InitialExcelSheet(exlpackage, ExcelFilePath, ExcelSheetName + trialCount.ToString(), out worksheet);
+            raw = 2;
+            col = 1;
+            var strName = ExcelFilePath.Split('/');
+            ScreenshotDemoName = strName[strName.Length - 1].Split('.')[0];
+        }
+
+        var gb = gazeTime.GetComponent<GazeTimeCalc>();
+        var gb2 = gazeRay.GetComponent<SRanipal_GazeRaySample>();
+
+        // Gaze Screenshot
+        if (IsRecording)
+        {
+            //DataRecordUtil.CaptureCamera(cam, rect);
+
+            /*Debug.Log("lastFrameGazeTime: " + lastGazeTime);
+            Debug.Log("gazeTime: " + gb.gazeTime);
+            Debug.Log("LastGazePixel: x:" + lastPixel.x + ", y:" + lastPixel.y);
+            Debug.Log("This-time GazePixel: x:" + gb2.Pixel_X + ",y:" + gb2.Pixel_Y);*/
+
+            if (lastGazeTime > gazeDurationThreshold && gb.gazeTime < gazeDurationThreshold)
+            {
+                // Take Screenshot
+                /** (Here I don't know why I save this frame but it seems screenshot of last frame got recorded, which caters to my willingness exactly -_-, well just let it go) **/
+                DataRecordUtil.TakeScreenshot(ScreenshotDirPath, ScreenshotDemoName, trialCount);
+                Debug.Log("Screenshot of the display is taken successfully!");
+            }
+        }
+        // Refresh
+        lastGazeTime = gb.gazeTime;
+        lastPixel.x = (float)gb2.Pixel_X;
+        lastPixel.y = (float)gb2.Pixel_Y;
+
+    }
+
+
+    // Initialize Excel File Sheet
+    public void InitialExcelSheet(ExcelPackage exlpkg, string excelFilePath, string sheetName, out ExcelWorksheet wksheet)
+    {
+        // Define Excel FilePath
+        string path = Application.dataPath + "/" + excelFilePath;
+
+        // If directory name doesn't exist, create one
+        string dirPath = System.IO.Path.GetDirectoryName(path);
+        if (!Directory.Exists(dirPath))
+        {
+            Directory.CreateDirectory(dirPath);
+        }
+
+        FileInfo newFile = new FileInfo(path);
+
+        // Open Excel File
+
+        int cnt = 1;
+        //ExcelWorksheet worksheet = null;
+        DataRecordUtil.SheetRespawn(exlpkg, sheetName, out wksheet);
+        //worksheet.Cells[1, cnt++].Value = "Frames";
+        wksheet.Cells[1, cnt++].Value = "TimeStamp(s)";
+        wksheet.Cells[1, cnt++].Value = "Gaze(X)";
+        wksheet.Cells[1, cnt++].Value = "Gaze(Y)";
+        wksheet.Cells[1, cnt++].Value = "Gaze Duration(s)";
+            
+        for(int i = 0; i < TrackerList.Length; i++)
+        {
+            var trackerName = TrackerList[i].name;
+            wksheet.Cells[1, cnt++].Value = trackerName + "(X)";
+            wksheet.Cells[1, cnt++].Value = trackerName + "(Y)";
+            wksheet.Cells[1, cnt++].Value = trackerName + "(Z)";
+            wksheet.Cells[1, cnt++].Value = trackerName + "(Raw)";
+            wksheet.Cells[1, cnt++].Value = trackerName + "(Pitch)";
+            wksheet.Cells[1, cnt++].Value = trackerName + "(Yaw)";
+        }
+        exlpkg.Save();
+        Debug.Log(sheetName + " is initialied.");
+    }
+
+    /*
+    private void RecordStart()
+    {
+        var gb = dataExtract.GetComponent<DataExtract>();
+        gb.startIcon.SetActive(true);
+        gb.stopIcon.SetActive(false);
+        Debug.Log("Recording On...");
+        gb.IsRecording = !gb.IsRecording;
+    }
+
+    private void RecordOff()
+    {
+        var gb = dataExtract.GetComponent<DataExtract>();
+        gb.startIcon.SetActive(false);
+        gb.stopIcon.SetActive(true);
+
+        Debug.Log("Recording Off...");
+        gb.IsRecording = !gb.IsRecording;
+        Debug.Log("Saving Data To Excel File!");
+        gb.exlpackage.Save();
+        Debug.Log("Data has been saved successfully!");
+    }
+    */
+}
---
+++ Assets/Scripts/Data Extraction/DataRecordUtil.cs
@@ -0,0 +1,190 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OfficeOpenXml;
+using System.IO;
+using UnityEditor;
+
+namespace ExperimentDataRecord {
+    public class DataRecordUtil : MonoBehaviour
+    {
+
+        /// <summary>
+        /// Write to Excel ; Needs OfficeOpenXml.dll;
+        /// </summary>
+        /// <param name="excelName">excelName</param>
+        /// <param name="sheetName">sheetName</param>
+        public static void WriteExcel(string raw, string col, string content, string excelName = "DataDir/demo.xlsx", string sheetName = "sheet1")
+        {
+            // Set Excel FilePath through Panel
+            // string outputDir = EditorUtility.SaveFilePanel("Save Excel", "", "New Resource", "xlsx");
+
+            // Define Excel FilePath
+            string path = Application.dataPath + "/" + excelName;
+
+            // If directory name doesn't exist, create one
+            string dirPath = System.IO.Path.GetDirectoryName(path);
+            if (!Directory.Exists(dirPath))
+            {
+                Directory.CreateDirectory(dirPath);
+            }
+
+            // Reference to Excel File with path, respawn one if file does not exist
+            FileInfo newFile = new FileInfo(path);
+
+            // Open Excel File
+            using (ExcelPackage exlpackage = new ExcelPackage(newFile))
+            {
+                // Choose Excel WorkSheet In Excel File
+                ExcelWorksheet worksheet = null;
+
+                if(SheetRespawn(exlpackage, sheetName, out worksheet))
+                {
+                    Debug.Log("Successfully add the sheet " + sheetName + " to the excel file");
+                }
+                else
+                {
+                    Debug.Log(sheetName + " already exists, use the origin one");
+                }
+
+                worksheet.Cells[int.Parse(raw), int.Parse(col)].Value = content;
+                
+                // Add column
+                /*worksheet.Cells[2, 1].Value = "ID";
+                worksheet.Cells[2, 2].Value = "Product";
+                worksheet.Cells[2, 3].Value = "Quantity";
+                worksheet.Cells[2, 4].Value = "Price";
+                worksheet.Cells[2, 5].Value = "Value";
+
+                // Add raw
+                worksheet.Cells["A2"].Value = 12001;
+                worksheet.Cells["B2"].Value = "Nails";
+                worksheet.Cells["C2"].Value = 37;
+                worksheet.Cells["D2"].Value = 3.99;*/
+
+                // Save Excel File
+                exlpackage.Save();
+            }
+        }
+
+        // Get the Excel Worksheet
+        public static bool SheetRespawn(ExcelPackage exlpackage, string sheetName, out ExcelWorksheet worksheet)
+        {
+            for (int i = 1; i <= exlpackage.Workbook.Worksheets.Count; i++)
+            {
+                if (exlpackage.Workbook.Worksheets[i].ToString().ToLower() == sheetName.ToLower())
+                {
+                    worksheet = exlpackage.Workbook.Worksheets[i];
+                    return false;
+                }
+            }
+            worksheet = exlpackage.Workbook.Worksheets.Add(sheetName);
+            return true;
+        }
+
+        // Delete the file with path given
+        public static bool DeleteFile(string path)
+        {
+            path = Application.dataPath + '/' + path;
+            FileInfo newFile = new FileInfo(path);
+            if(newFile.Exists)
+            {
+                newFile.Delete();
+                //FileUtil.DeleteFileOrDirectory(path);
+                //AssetDatabase.Refresh();
+                Debug.Log("Successfully delete the file: " + path);
+                return true;
+            }
+            Debug.Log("File not found in this path: " + path);
+            return false;
+        }
+
+        // Delete the Directory with path given
+        public static bool DeleteDirectory(string target_dir)
+        {
+            target_dir = Application.dataPath + '/' + target_dir;
+            
+            if (Directory.Exists(target_dir))
+            {
+                string[] files = Directory.GetFiles(target_dir);
+                string[] dirs = Directory.GetDirectories(target_dir);
+
+                foreach (string file in files)
+                {
+                    File.SetAttributes(file, FileAttributes.Normal);
+                    File.Delete(file);
+                }
+
+                foreach (string dir in dirs)
+                {
+                    DeleteDirectory(dir);
+                }
+
+                Directory.Delete(target_dir, false);
+                Debug.Log("Successfully delete the directory " + target_dir);
+                return true;
+            }
+            else
+            {
+                Debug.Log("Filepath: " + target_dir + " is not found");
+                return false;
+            }
+        }
+
+
+        // Take the screenshot
+        public static void TakeScreenshot(string dirPath, string filedemoName, int cnt)
+        {
+            dirPath = Application.dataPath + "/" + dirPath;
+            if (!Directory.Exists(dirPath))
+            {
+                Directory.CreateDirectory(dirPath);
+            }
+
+            // Take Screenshot
+            ScreenCapture.CaptureScreenshot(dirPath + "/" + filedemoName + "_" + cnt.ToString() + '_' + Time.frameCount.ToString() + ".jpg");
+
+        }
+
+        /// <summary>
+        /// 对相机截图。 
+        /// </summary>
+        /// <returns>The screenshot2.</returns>
+        /// <param name="camera">Camera.要被截屏的相机</param>
+        /// <param name="rect">Rect.截屏的区域</param>
+        public static Texture2D CaptureCamera(Camera camera, Rect rect)
+        {
+            // 创建一个RenderTexture对象
+            RenderTexture rt = new RenderTexture((int)rect.width, (int)rect.height, 0);
+            // 临时设置相关相机的targetTexture为rt, 并手动渲染相关相机
+            camera.targetTexture = rt;
+            camera.Render();
+            //ps: --- 如果这样加上第二个相机，可以实现只截图某几个指定的相机一起看到的图像。
+            //ps: camera2.targetTexture = rt;
+            //ps: camera2.Render();
+            //ps: -------------------------------------------------------------------
+
+            // 激活这个rt, 并从中中读取像素。
+            RenderTexture.active = rt;
+            Texture2D screenShot = new Texture2D((int)rect.width, (int)rect.height, TextureFormat.RGB24, false);
+            screenShot.ReadPixels(rect, 0, 0);// 注：这个时候，它是从RenderTexture.active中读取像素
+            screenShot.Apply();
+
+            // 重置相关参数，以使用camera继续在屏幕上显示
+            camera.targetTexture = null;
+            //ps: camera2.targetTexture = null;
+            RenderTexture.active = null; // JC: added to avoid errors
+            GameObject.Destroy(rt);
+            // 最后将这些纹理数据，成一个png图片文件
+            byte[] bytes = screenShot.EncodeToPNG();
+            string filename = Application.dataPath + "/ScreenShot/snapshot.png";
+            System.IO.File.WriteAllBytes(filename, bytes);
+            Debug.Log(string.Format("截屏了一张照片: {0}", filename));
+
+            return screenShot;
+        }
+    }
+
+    
+}
+
