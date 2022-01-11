9b95712eca41168cf89354192c56e143ec4e3a1a
Zhuoyun Zhong <zzy905954450@gmail.com>
2022-01-11 15:23:20 -0500

    merge gaze scripts

--- Assets/Scripts/Eye Tracking/GazeInfo.cs
+++ Assets/Scripts/Eye Tracking/GazeInfo.cs
@@ -4,26 +4,45 @@ using UnityEngine;
 using UnityEngine.UI;
 using ViveSR.anipal.Eye;
 
-public class GazeInfoDisp : MonoBehaviour
+public class GazeInfo : MonoBehaviour
 {
     public SRanipal_GazeRaySample gazeRay;
     public Text pixelText;
     public Text durationText;
+    public LineRenderer lineRenderer;
 
-    private Vector3 prevPoint;
+    private float range;
+    private float distance;
+    private int segments;
+    private float deltaAngle;
+
+    private Vector2 currPoint;
+    private Vector2 prevPoint;
     private System.DateTime prevTime;
     private System.TimeSpan timeSpan;
-    
-    public GameObject Range;
 
     private bool dispGaze;
     private bool dispText;
 
     void Start()
     {
+        // Initialization
+        currPoint = new Vector2(0.0f, 0.0f);
         prevPoint = new Vector2(0.0f, 0.0f);
         prevTime = System.DateTime.Now;
         timeSpan = System.DateTime.Now.Subtract(prevTime);
+
+        // Gaze info parameter
+        dispGaze = true;
+        dispText = true;
+        // circle
+        range = 3.0f;
+        distance = 99.0f;
+        segments = 18;
+        lineRenderer.positionCount = segments + 2;
+        deltaAngle = 2*Mathf.PI / segments;
+        // hide ray
+        gazeRay.GazeRayRenderer.enabled = false;
     }
 
     void Update()
@@ -34,30 +53,29 @@ public class GazeInfoDisp : MonoBehaviour
 
     private void UpdateGazeInfo()
     {
-        var point = new Vector2((float)gazeRay.inter_x, (float)gazeRay.inter_y);
-        var range = Range.GetComponent<GazeRangeShow>();
+        currPoint = new Vector2((float)gazeRay.inter_x, (float)gazeRay.inter_y);
 
         // Point in Range - calculate timespan
-        if (PointInRange(prevPoint, new Vector2(range.x_radius, range.y_radius), point))
+        if (PointInRange(prevPoint, currPoint, distance))
         {
             timeSpan = System.DateTime.Now.Subtract(prevTime);
         }
         // If out of range - reset
         else
         {
-            prevPoint = point;
+            prevPoint = currPoint;
             prevTime = System.DateTime.Now;
         }
     }
 
-    private bool PointInRange(Vector2 origin, Vector2 range, Vector2 target)
+    private bool PointInRange(Vector2 origin, Vector2 target, float range)
     {
         // Compute distance
         float x_square = (origin.x - target.x) * (origin.x - target.x);
         float y_square = (origin.y - target.y) * (origin.y - target.y);
         // Check distance
-        float ans = x_square / (range.x * range.x) + y_square / (range.y * range.y);
-        return (ans <= 1)? true : false;
+        float ans = (range * range) - (x_square + y_square);
+        return (ans >= 0)? true : false;
     }
 
     private void DisplayInfo()
@@ -66,7 +84,7 @@ public class GazeInfoDisp : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.G))
         {
             dispGaze = !dispGaze;
-            
+            lineRenderer.enabled = dispGaze;
         }
         if (Input.GetKeyDown(KeyCode.T))
         {
@@ -74,11 +92,12 @@ public class GazeInfoDisp : MonoBehaviour
             pixelText.gameObject.SetActive(dispText);
             durationText.gameObject.SetActive(dispText);
         }
-            
+        
         // Display
         if (dispGaze)
         {
             // display gaze
+            DrawCircle(currPoint, range);
         }
 
         if (dispText)
@@ -86,11 +105,29 @@ public class GazeInfoDisp : MonoBehaviour
             // display pixel
             float x = (float)gazeRay.Pixel_X;
             float y = (float)gazeRay.Pixel_Y;
-            pixelText.text = "Pixel Coordinates: (" + x.ToString("0.0") + ", " + y.ToString("0.0") + ")";
+            pixelText.text = "Pixel Coordinates: (" + x.ToString("0.0") + ", " + 
+                                                      y.ToString("0.0") + ")";
 
             // display duration
-            float gazeTime = float.Parse(timeSpan.Seconds.ToString() + "." + timeSpan.Milliseconds.ToString()); 
+            float gazeTime = float.Parse(timeSpan.Seconds.ToString() + "." + 
+                                         timeSpan.Milliseconds.ToString()); 
             durationText.text = "Gaze Stopping Time: " + gazeTime.ToString("0.0") + "s";
+        }
+    }
+
+    private void DrawCircle(Vector2 point, float radius)
+    {
+        float x;
+        float y;
+
+        float angle = 0.0f;
+        for (int i = 0; i < (segments + 2); i++)
+        {
+            x = Mathf.Cos(angle) * range + point.x;
+            y = Mathf.Sin(angle) * range + point.y;
+            lineRenderer.SetPosition(i, new Vector3(x, y, distance));
+
+            angle += deltaAngle;
         }
     }
 }
